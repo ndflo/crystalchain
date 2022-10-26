19adb93b573b23189fd457a2f3f76111a6a28b0b
ndflo <43345785+ndflo@users.noreply.github.com>
2022-10-26 07:48:02 +0200

    Update ALGORITHM.py

--- ALGORITHM.py
+++ ALGORITHM.py
@@ -1,6 +1,6 @@
 
-from numpy import append
-
+import numpy as np
+import pandas as pd
 
 
 
@@ -17,54 +17,14 @@ def entryTime(s, keypad):
     for num in keypad:
         current_location = num
         try:
-            abs(num) <= 9
+            assert abs(num) <= 9
         except:
             print("We found an issu with the file keypad")
             break
     
-    entry_time = keypad
+    #get the array
+    keypad = np.array(keypad)
+    new_keypad = keypad.reshape((3,3))
 
-    for num in keypad:
-        current_location = num
-        if current_location == keypad[0]:
-            entry_time[1], entry_time[3], entry_time[4] = 1
-            list_of_number_to_add.append[1]
-            continue
-        else:
-            entry_time[2], entry_time[5], entry_time[6], entry_time[7], entry_time[8] = 2
-            list_of_number_to_add.append[2]
-            continue
 
-        if current_location == keypad[1]:
-            list_of_number_to_add.append[1]
-            entry_time[0], entry_time[2], entry_time[3], entry_time[4], entry_time[5] = 1
-            continue
-        else:
-            entry_time[6], entry_time[7], entry_time[8] = 2
-            list_of_number_to_add.append[2]
-            continue
-        
-    
-        """
-        example for the rest
-
-        if current_location == keypad[3]:
-            list_of_number_to_add.append[1]
-            entry_time[0]
-            continue
-        else:
-            entry_time[6]
-            continue
-        """
-    
-
-    #lopp for add each element of the list list_of_number_to_add
-    time_taken = 0
-    for number in list_of_number_to_add :
-        time_taken += number
-    
-    return time_taken
-
-s = [4,2,3,6,9,2  ]         
-keypad = [9,2,3,8,5,7,6,1,4]
-entryTime(s, keypad)
+    #
